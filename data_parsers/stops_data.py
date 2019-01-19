a0eacd6c68d94c4dcd1d53a8ec90c92c868cf14c
dan soucy <dev@danso.ca>
2019-01-19 18:46:32 -0500

    add coords to stops parser

--- data_parsers/stops_data.py
+++ data_parsers/stops_data.py
@@ -3,7 +3,8 @@ from local_data_dir import data_dir
 with open(data_dir + "gtfs_stm/stops.txt") as stops_file:
     print("skip first line: ", stops_file.readline())
     for line in stops_file:
-        stop_id, stop_code, stop_name, unused, unused, unused, unused, unused, unused = line.split(",")
+        stop_id, stop_code, stop_name, stop_lat, stop_lon, unused, unused, unused, unused = line.split(",")
         print("stop_id:", stop_id)
         print("stop_code:", stop_code)
         print("stop_name:", stop_name)
+        print("coords: " + stop_lat + ", " + stop_lon)
