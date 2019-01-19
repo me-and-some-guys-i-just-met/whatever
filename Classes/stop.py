b6135ee9a31b7f00f8cccc40c4aa8ea56ab1d1d0
dan soucy <dev@danso.ca>
2019-01-19 18:05:21 -0500

    touch up stop.py

--- Classes/stop.py
+++ Classes/stop.py
@@ -1,10 +1,10 @@
 class Stop:
 
-    def __init__(self, stop_id = None, stop_code = None, stop_name = None, lat = None, long = None, busses = None):
+    def __init__(self, stop_id = None, code = None, name = None, lat = None, lon = None, busses = None):
         self.stop_id = stop_id
-        self.stop_code = stop_code
-        self.stop_name = stop_name
-        self.coordinates = [lat, long]
+        self.code = code
+        self.name = name
+        self.coordinates = (lat, lon)
         self.busses = busses
 
     #Might need to extend this class in the future
