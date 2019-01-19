e95190458a5aa6a80ea3a20e07bdccf6055d1090
Noam cohen <cohen.noam@hotmail.com>
2019-01-19 18:34:12 -0500

    added read pop

---
+++ read pop.py
@@ -0,0 +1,18 @@
+from Classes.arrondissement import Arrondissement
+from data_parsers.local_data_dir import data_dir
+
+burrows=dict()
+burrows_object=dict()
+with open("Classes/population-quartiers-anciens-territoires-administratifs.csv") as pop_file:
+  for i in range(5):
+    pop_file.readline()
+  for line in pop_file:
+      #print(line.split(','))
+      name, unused, popu = line.strip().split(',')
+      burrows[name]=popu
+
+
+for i in burrows:
+    burrows_object[i]=Arrondissement(i,0,0,burrows.get(i))
+
+print(burrows_object)
