dddefcd103a3bafae28a83d7050bd643c1bdbfff
dan soucy <dev@danso.ca>
2019-01-19 17:15:52 -0500

    remove debugging output

--- data_parsers/trips_data.py
+++ data_parsers/trips_data.py
@@ -3,7 +3,6 @@ from local_data_dir import data_dir
 with open(data_dir + "gtfs_stm/trips.txt") as trips_file:
     print("skip first line: ", trips_file.readline())
     for line in trips_file:
-        print(line)
         route_id, unused, trip_id, unused, unused, unused, unused, unused, unused = line.split(",")
         print("route_id:", route_id)
         print("trip_id:", trip_id)
