f483ba721446ee2af28b93e97a0f423cb67fac2e
dan soucy <dev@danso.ca>
2019-01-19 17:59:55 -0500

    add stop_code to stop class

--- Classes/stop_class.py
+++ Classes/stop_class.py
@@ -1,9 +1,10 @@
-class Stop(object):
+class Stop:
 
-    def __init__(self, stop_id = None, stop_name = None, lat = None, long = None, busses = None):
+    def __init__(self, stop_id = None, stop_code = None, stop_name = None, lat = None, long = None, busses = None):
         self.stop_id = stop_id
+        self.stop_code = stop_code
         self.stop_name = stop_name
         self.coordinates = [lat, long]
         self.busses = busses
 
-    #Might need to extend this class in the future 
+    #Might need to extend this class in the future
