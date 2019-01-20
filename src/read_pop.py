078ec4d77ab0553561772ae97563317d9602d26c
dan soucy <dev@danso.ca>
2019-01-19 21:15:24 -0500

    clean indentation

--- src/read_pop.py
+++ src/read_pop.py
@@ -7,12 +7,12 @@ area = "FIXME"
 burrows = dict()
 
 with open("../data/population-quartiers-anciens-territoires-administratifs.csv") as pop_file:
-  for i in range(5):
-    pop_file.readline()
-  for line in pop_file:
-      name, unused, popu = line.strip().split(',')
-      name = name.strip('"')
-      burrows[name] = Arrondissement(name, stops, area, popu)
+    for i in range(5):
+        pop_file.readline()
+    for line in pop_file:
+        name, unused, popu = line.strip().split(',')
+        name = name.strip('"')
+        burrows[name] = Arrondissement(name, stops, area, popu)
 
 for boro in burrows:
     print(boro + ": " + str(burrows.get(boro)))
