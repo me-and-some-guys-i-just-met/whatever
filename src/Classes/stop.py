0f81db192068783a2617ee3067f9f61ce6a9d631
dan soucy <dev@danso.ca>
2019-01-20 04:30:59 -0500

    repair stop.py

--- src/Classes/stop.py
+++ src/Classes/stop.py
@@ -1,12 +1,10 @@
 class Stop:
-'''
+    '''
 Represents a bus stop
-'''
-    def __init__(self, stop_id = None, code = None, name = None, lat = None, lon = None, busses = None):
+    '''
+    def __init__(self, stop_id = None, lat = None, lon = None):
         self.stop_id = stop_id
-        self.code = code
-        self.name = name
-        self.coordinates = (lat, lon)
-        self.busses = busses
+        self.lat = lat
+        self.lon = lon
 
     #Might need to extend this class in the future
