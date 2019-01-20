f820fd88a9a859745ad60e0eb1511c1c65868132
dan soucy <dev@danso.ca>
2019-01-19 21:02:57 -0500

    rename "size" to "area"

--- src/Classes/arrondissement.py
+++ src/Classes/arrondissement.py
@@ -1,13 +1,13 @@
 class Arrondissement:
 
-    def __init__(self, name, stops, size, population):
+    def __init__(self, name, stops, area, population):
         self.name = name
         self.stops = stops
-        self.size = size
+        self.area = area
         self.population = population
 
     def __repr__(self):
         return self.name + " has population " + str(self.population)
 
     def density(self):
-        return population / size
+        return population / area
